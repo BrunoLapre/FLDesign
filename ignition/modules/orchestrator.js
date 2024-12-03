c62111c1328c9b85188df4da50a9fb40ba08c679
Bruno Lapré <bruno@lap.re>
2024-12-03 21:24:05 +0100

    Cleaned

--- ignition/modules/orchestrator.js
+++
@@ -1,8 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("Orchestrator", (m) => {
-  const tokens = m.useModule(require("./tokens"));
-  const orchestrator = m.contract("FLOrchestrator");
-
-  return { orchestrator };
-});
